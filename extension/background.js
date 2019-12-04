2d1cb9eb3d9b26fe5a03805c361b74e699df2b5d
Zach Resnick <zachary.l.resnick@gmail.com>
2019-12-04 10:32:47 -0500

    Feat: Icon toggle works

--- extension/background.js
+++ extension/background.js
@@ -1,16 +1,24 @@
-import sites from './sites'
+const sites = {
+  // allrecipes: true,
+  // chowhound: true,
+  epicurious: true,
+  foodnetwork: true
+  // simplyrecipes: true
+}
 
 chrome.tabs.onUpdated.addListener(toggleIcon)
+chrome.tabs.onUpdated.addListener(() => {
+  console.log('HIIIIII')
+})
 chrome.tabs.onActivated.addListener(toggleIcon)
 
 function toggleIcon() {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     let url = tabs[0].url,
       site = ''
-    console.log(url, site)
     const urlTail = url.split('www.')[1]
     if (urlTail) site = urlTail.split('.com')[0]
-    if (url && scraper[site]) {
+    if (url && sites[site]) {
       chrome.browserAction.setIcon({path: 'enabled.png'})
     } else {
       chrome.browserAction.setIcon({path: 'disabled.png'})
--- extension/sites.js
+++
@@ -1,9 +0,0 @@
-const sites = {
-  // allrecipes: true,
-  // chowhound: true,
-  epicurious: true,
-  foodnetwork: true
-  // simplyrecipes: true
-}
-
-export default sites
