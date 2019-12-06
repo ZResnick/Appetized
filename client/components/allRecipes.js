0d5f2840d0d64602ae832ab17131a34c30e25c46
Zach Resnick <zachary.l.resnick@gmail.com>
2019-12-06 11:47:03 -0500

    Updates Navbar and begins styling on it

--- client/components/allRecipes.js
+++ client/components/allRecipes.js
@@ -2,34 +2,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {getRecipes, addNewRecipe} from '../store/recipes'
+import {getRecipes} from '../store/recipes'
 import {Card, Image} from 'semantic-ui-react'
 
 class AllRecipes extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      url: ''
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  }
-
   componentDidMount() {
     this.props.getRecipes()
-  }
-
-  handleChange() {
-    this.setState({[event.target.name]: event.target.value})
-  }
-
-  handleSubmit() {
-    event.preventDefault()
-    let temp = this.state.url
-    this.setState({
-      url: ''
-    })
-    this.props.addNewRecipe(temp)
   }
 
   render() {
@@ -38,19 +16,6 @@ class AllRecipes extends React.Component {
 
     return (
       <div>
-        <form className="forms" onSubmit={this.handleSubmit}>
-          <label htmlFor="url">URL: </label>
-          <input
-            placeholder="Recipe URL"
-            type="text"
-            name="url"
-            value={this.state.url}
-            onChange={this.handleChange}
-          />
-          <button type="submit" disabled={!this.state.url}>
-            Submit
-          </button>
-        </form>
         {!recipes.length ? (
           <h1>It doesn't look like you have any recipes.</h1>
         ) : (
@@ -87,9 +52,6 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   getRecipes: () => {
     dispatch(getRecipes())
-  },
-  addNewRecipe: url => {
-    dispatch(addNewRecipe(url))
   }
 })
 
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,42 +3,106 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {addNewRecipe} from '../store/recipes'
+import {Form, Dropdown, Menu, Image} from 'semantic-ui-react'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div className="mainNav">
-    <div>
-      <Link to={isLoggedIn ? '/allRecipes' : '/home'}>
-        <h1 className="navbar-main-title">Appetized</h1>
-      </Link>
-    </div>
-    <div>
-      <nav>
-        {isLoggedIn ? (
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      url: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange() {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit() {
+    event.preventDefault()
+    let temp = this.state.url
+    this.setState({
+      url: ''
+    })
+    this.props.addNewRecipe(temp)
+  }
+
+  render() {
+    const {isLoggedIn, handleClick, firstName, lastName, imageUrl} = this.props
+
+    const trigger = (
+      <span>
+        <Image avatar src={imageUrl} />
+        {`Hi, ${firstName}`}
+        {'    '}
+      </span>
+    )
+
+    return (
+      <div className="mainNav">
+        <div className="left-side-of-navbar">
           <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/allRecipes">All Recipes</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
+            <Link to={isLoggedIn ? '/allRecipes' : '/home'}>
+              <h1 className="navbar-main-title">Appetized</h1>
+            </Link>
           </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+          <div className="add-recipe-nav-form">
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field>
+                <label htmlFor="url"></label>
+                <input
+                  placeholder="Add a recipe URL here"
+                  type="text"
+                  name="url"
+                  value={this.state.url}
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+            </Form>
           </div>
-        )}
-      </nav>
-    </div>
-  </div>
-)
+        </div>
+        <div>
+          <div className="main-nav-dropdown">
+            <Menu>
+              <Dropdown
+                size="massive"
+                trigger={trigger}
+                pointing
+                className="link item"
+              >
+                <Dropdown.Menu>
+                  <Dropdown.Header>Categories</Dropdown.Header>
+                  <Dropdown.Item>Home Goods</Dropdown.Item>
+                  <Dropdown.Item>Bedroom</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Order</Dropdown.Header>
+                  <Dropdown.Item>Status</Dropdown.Item>
+                  <Dropdown.Item>
+                    <a href="#" onClick={handleClick}>
+                      Logout
+                    </a>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    firstName: state.user.firstName,
+    lastName: state.user.lastName,
+    imageUrl: state.user.imgUrl
   }
 }
 
@@ -46,6 +110,9 @@ const mapDispatch = dispatch => {
   return {
     handleClick() {
       dispatch(logout())
+    },
+    addNewRecipe: url => {
+      dispatch(addNewRecipe(url))
     }
   }
 }
