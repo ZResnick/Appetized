a8e6c12c522e7bfce3c51ded156b5f37f115cc5c
Zach Resnick <zachary.l.resnick@gmail.com>
2019-12-10 14:33:58 -0500

    Grocery List model and route is added

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./routes/users'))
 router.use('/recipes', require('./routes/recipes'))
 router.use('/folders', require('./routes/folders'))
+router.use('/groceryList', require('./routes/groceryList'))
 
 router.use((req, res, next) => {
   const error = new Error('Not found.')
