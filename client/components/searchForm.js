6aa50975f1360426148affaeb60d746684744191
Zach Resnick <zachary.l.resnick@gmail.com>
2020-03-20 13:32:16 -0400

    Adds Search Bar!!!

--- client/components/searchForm.js
+++ client/components/searchForm.js
@@ -1,36 +1,79 @@
+import _ from 'lodash'
 import React, {Component} from 'react'
+import {Search, Grid, Header, Segment} from 'semantic-ui-react'
 import {connect} from 'react-redux'
 import {getSearchedByTitle} from '../store/recipes'
-import {Form, Dropdown, Image, Icon} from 'semantic-ui-react'
 
-export class SearchForm extends Component {
+class SearchForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchQuery: ''
+      isLoading: false,
+      results: [],
+      value: ''
     }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmitSearch = this.handleSubmitSearch.bind(this)
+    this.resetComponent = this.resetComponent.bind(this)
+    this.handleResultSelect = this.handleResultSelect.bind(this)
+    this.handleSearchChange = this.handleSearchChange.bind(this)
   }
 
-  handleChange() {
-    this.setState({[event.target.name]: event.target.value})
+  componentWillMount() {
+    this.resetComponent()
   }
 
-  handleSubmitSearch() {
-    event.preventDefault()
-    let temp = this.state.searchQuery
-    this.setState({
-      searchQuery: ''
-    })
-    this.props.getSearchedByTitle(temp)
+  resetComponent = () => {
+    this.setState({isLoading: false, results: [], value: ''})
+  }
+
+  handleResultSelect = (e, {result}) => {
+    this.setState({value: result.title})
+    this.resetComponent()
+  }
+
+  handleSearchChange = (e, {value}) => {
+    this.setState({isLoading: true, value})
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.resetComponent()
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = result => re.test(result.title)
+
+      this.props.getSearchedByTitle(this.state.value)
+      let recipes = this.props.searchedByTitle.slice(0, 5)
+
+      //apparently the search function overrides the id with 0,1,2,3 so the below adds a new key:value to persist the realID
+      recipes.forEach(recipe => {
+        recipe.realID = recipe.id
+      })
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(recipes, isMatch)
+      })
+    }, 300)
   }
 
   render() {
-    const {searchedByTitle} = this.props
+    const {isLoading, value, results} = this.state
     return (
-      <div>
-        <h1>Hello {searchedByTitle}</h1>
+      <div className="search-bar-in-nav">
+        <Grid>
+          <Grid.Column width={6}>
+            <Search
+              placeholder="Search recipes here"
+              size="big"
+              loading={isLoading}
+              onResultSelect={this.handleResultSelect}
+              onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                leading: true
+              })}
+              results={results}
+              value={value}
+              {...this.props}
+            />
+          </Grid.Column>
+        </Grid>
       </div>
     )
   }
