e087612bfcbc27624446bdee892ef6e30cdfd832
Zach Resnick <zachary.l.resnick@gmail.com>
2020-03-27 14:32:13 -0400

    adds thunks and stuff for edit folder and delete folder

--- client/store/folders.js
+++ client/store/folders.js
@@ -6,6 +6,8 @@ import history from '../history'
  */
 const GOT_ALL_FOLDERS = 'GOT_ALL_FOLDERS'
 const GOT_SINGLE_FOLDER = 'GOT_SINGLE_FOLDER'
+const UPDATED_FOLDER_NAME = 'UPDATED_FOLDER_NAME'
+const DELETED_FOLDER = 'DELETED_FOLDER'
 
 /**
  * INITIAL STATE
@@ -17,6 +19,14 @@ const initialState = {folders: [], singleFolder: {}}
  */
 const gotAllFolders = folders => ({type: GOT_ALL_FOLDERS, folders})
 const gotSingleFolder = folder => ({type: GOT_SINGLE_FOLDER, folder})
+const deletedFolder = deletedFolderId => ({
+  type: DELETED_FOLDER,
+  deletedFolderId
+})
+const updatedFolderName = updatedFolder => ({
+  type: UPDATED_FOLDER_NAME,
+  updatedFolder
+})
 
 /**
  * THUNK CREATORS
@@ -34,6 +44,28 @@ export const getSingleFolder = id => async dispatch => {
   try {
     const {data} = await axios.get(`/api/folders/${id}`)
     dispatch(gotSingleFolder(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const deleteFolder = id => async dispatch => {
+  try {
+    await axios.put(`/api/folders/deleteFolder/${id}`)
+    dispatch(deletedFolder(id))
+    history.push('/')
+    history.push(`/recipeBox`)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const updateFolder = (id, title) => async dispatch => {
+  try {
+    const {data} = await axios.put(`/api/folders/changeName/${id}`, {title})
+    dispatch(updatedFolderName(data))
+    history.push('/')
+    history.push(`/recipeBox/folder/${id}/page/1`)
   } catch (err) {
     console.error(err)
   }
@@ -81,6 +113,18 @@ export default function(state = initialState, action) {
       return {...state, folders: action.folders}
     case GOT_SINGLE_FOLDER:
       return {...state, singleFolder: action.folder}
+    case DELETED_FOLDER:
+      return {
+        ...state,
+        folders: state.folders.filter(
+          folder => folder.id !== action.deletedFolderId
+        )
+      }
+    case UPDATED_FOLDER_NAME:
+      return {
+        ...state,
+        singleFolder: action.updatedFolder
+      }
     default:
       return state
   }
